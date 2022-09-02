cd51630126e0af921dbb7923d2137740d1347133
leizzer <lizzydw@gmail.com>
2022-09-02 02:57:26 -0300

    Works Index
    - Basic Accordion
    - Style changes

--- front/src/components/quotes/index.jsx
+++ front/src/components/quotes/index.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import styled from 'styled-components'
 import StoreContext from '../../store'
 import sanitize from 'sanitize-html'
@@ -29,10 +29,32 @@ const Work = styled.p`
   margin-top: 1rem;
 `
 
+const Collapsable = styled.div`
+  color: #fbd88d;
+  text-align: left;
+  font-size: 1.5rem;
+  text-indent: 2rem;
+  margin-bottom: 0.5rem;
+  background-color: ${(props) => props.selected ? '#1c5772' : '#324d5b' };
+  transition: background-color 400ms linear;
+`
+const CollapsableContent = styled.div`
+  margin-top: 1rem;
+  display: ${(props) => props.show ? 'block' : 'none'};
+`
+
 const List = () => {
   const {store: {quotes, query}} = useContext(StoreContext)
+  const [selected, setSelected] = useState()
 
-  const prepareSnippet = (text) => {
+  const isSelected = (index) => index === selected
+
+  const toggleSelect = (index) => {
+    let i =  index === selected ? null : index
+    setSelected(i)
+  }
+
+  const processText = (text) => {
     const regex = new RegExp(`(?:\r\n|\r|\n|${query})`, 'gi')
     return sanitize(text).replace(
       regex,
@@ -40,13 +62,22 @@ const List = () => {
     )
   }
 
+  useEffect(() => {
+    selected && document.getElementById(selected).scrollIntoView()
+  }, [selected])
+
   const list = quotes.map((quote, i) => (
-    quote.Snippets.map((snippet, j) => (
-      <Quote key={`${i}-${j}`}>
-        <Snippet  dangerouslySetInnerHTML={{__html: prepareSnippet(snippet)}} />
-        <Work>{quote.Work}</Work>
-      </Quote>
-    ))
+    <Collapsable key={i} id={i} selected={isSelected(i)} onClick={() => toggleSelect(i)}>
+      {quote.Work} <span className="highlighted">({quote.Snippets.length})</span>
+
+      <CollapsableContent show={isSelected(i)}>
+        {isSelected(i) && quote.Snippets.map((snippet, j) => (
+          <Quote key={`${i}-${j}`}>
+            <Snippet  dangerouslySetInnerHTML={{__html: processText(snippet)}} />
+          </Quote>
+        ))}
+      </CollapsableContent>
+    </Collapsable>
   ))
 
   return (
